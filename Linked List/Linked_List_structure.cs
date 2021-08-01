770ef9417b53c70a44ccc172900ed1b4b6218d25
ananth118 <87264034+ananth118@users.noreply.github.com>
2021-08-01 13:20:02 +0530

    Add files via upload

---
+++ Linked List/Linked_List_structure.cs
@@ -0,0 +1,16 @@
+﻿class LinkedList {
+    // The first node(head) of the linked list
+    // Will be an object of type Node (null by default)
+    Node head;
+ 
+    class Node {
+        int data;
+        Node next;
+ 
+        // Constructor to create a new node
+        Node(int d)
+         {
+              data = d;
+         }
+    }
+}
